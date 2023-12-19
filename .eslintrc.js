e7dc9a792bb3c72b33e2d2ea641293ba9901e2d8
Angus McConnell <angusmcconnell101@gmail.com>
2023-12-19 17:42:59 +0000

    Update favicon and site title

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/**
+ * This is intended to be a basic starting point for linting in your app.
+ * It relies on recommended configs out of the box for simplicity, but you can
+ * and should modify this configuration to best suit your team's needs.
+ */
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  // Base config
+  extends: [    
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "prettier"
+  ],
+};

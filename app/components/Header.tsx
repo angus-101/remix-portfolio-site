ccf1f01e4e949befe9dbe3eaf6dd92d1c0b24bf1
Angus McConnell <angusmcconnell101@gmail.com>
2023-12-22 15:38:41 +0000

    Created header componenet and imported logo

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+import logo from "../images/logo.png";
+
+export const Header = () => (
+    <div>
+        <img src={logo} alt="Website logo. Capital M with a line across the left half to look like a capital A."/>
+    </div>
+)
--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-slate-900">
         <Outlet />
         <ScrollRestoration />
         <Scripts />

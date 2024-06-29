39edbbb7e1e072f1a64d4e67f8f24df73672868a
angus-101 <angusmcconnell101@gmail.com>
2024-06-29 15:59:24 +0100

    add alt text to logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,7 +12,11 @@ interface HeaderProps {
 export const Header = ({ headerLinks }: HeaderProps) => (
     <div className='header'>
         <a href='/'>
-            <img src={logo} className='header--img' />
+            <img 
+                src={logo} 
+                className='header--img' 
+                alt='The logo of the website'
+            />
         </a>
         <div className='header--link-container'>
             {headerLinks.map((headerLink) => (

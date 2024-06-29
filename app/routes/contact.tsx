6b51caba5f5b9946fe8fd0ecb56fc34d4d7a5569
angus-101 <angusmcconnell101@gmail.com>
2024-06-29 16:45:20 +0100

    add meta function to routes

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,3 +1,11 @@
+import { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+    return [
+      { title: "Contact - Angus's Portfolio" }
+    ];
+  };
+
 export default function Contact() {
     return (
         <h1>
